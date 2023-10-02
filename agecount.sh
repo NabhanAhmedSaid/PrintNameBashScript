3e2310e5a854a56b76ed39696c9fe3b93205c409
NabhanAhmedSaid <100470531+NabhanAhmedSaid@users.noreply.github.com>
2023-10-02 13:54:25 -0400

    Add files via upload

---
+++ agecount.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo Tell me your name ?
+read NAME
+echo Tell me your last name ?
+read LAST
+
+echo Ok, Enter your year of birthday
+read NUMBER
+
+echo You are $NAME $LAST and you born in $NUMBER
